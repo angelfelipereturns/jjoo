b7beb958de4f9a8c96bfe9ffc8d396f44ad849e1
afaf <faded662@gmail.com>
2018-10-29 12:13:47 +0100

    Cambios en README y añadido script sql

---
+++ scripts/table_hibernate_sequence.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+);
+
+INSERT INTO hibernate_sequence (next_val) VALUES (16);
+INSERT INTO hibernate_sequence (next_val) VALUES (18);
+INSERT INTO hibernate_sequence (next_val) VALUES (20);

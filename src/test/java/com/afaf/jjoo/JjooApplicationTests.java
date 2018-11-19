7a223a5c3fadced51141ce3acb426f9692bd98ea
afaf <faded662@gmail.com>
2018-11-19 04:28:05 +0100

    test application

--- src/test/java/com/afaf/jjoo/JjooApplicationTests.java
+++ src/test/java/com/afaf/jjoo/JjooApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.afaf.jjoo.controller.JjooController;
+import com.afaf.jjoo.rest.ResumenSedesRestController;
+import com.afaf.jjoo.service.JjooService;
 
 /**
  * Clase test para el contexto de la aplicacion
@@ -20,10 +22,19 @@ public class JjooApplicationTests {
 	
 	@Autowired
 	private JjooController jjooController;
+	
+	@Autowired
+	private JjooService jjooService;
+	
+	@Autowired
+	private ResumenSedesRestController resumenSedesRestController;
+	
 
 	@Test
 	public void contextLoads() {
 		assertThat(jjooController).isNotNull();
+		assertThat(jjooService).isNotNull();
+		assertThat(resumenSedesRestController).isNotNull();
 	}
 
 }
